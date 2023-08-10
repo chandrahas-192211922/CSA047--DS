a9c4cc1d4ebc51beb8a4ea27fd17bb487f5488f5
chandrahas-192211922 <141902350+chandrahas-192211922@users.noreply.github.com>
2023-08-10 12:26:34 +0530

    Create generation of numbers.c

---
+++ generation of numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("enter the number of terms");
+    scanf("%d",&n);
+    printf("enter the number series");
+    for(i=1;i<=n;i++){
+       printf("%d",i);
+    }
+    return 0;
+}
